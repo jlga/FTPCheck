7fa43be47d9dda8cea0b469d96d8a066700b920d
juliusgau <juliusgau99@gmail.com>
2016-09-30 20:51:41 +0200

    Added Settings, Persistent Numbers and counting

--- FTPCheck/Program.cs
+++ FTPCheck/Program.cs
@@ -14,9 +14,15 @@ namespace FTPCheck
 {
     class Program
     {
+        //Config
+        const int THREAD_AMOUNT = 64;
+
+        
         IEnumerable<string> lines;
         string inputFile = "input.txt";
         string outputFile = "output.txt";
+
+        int okservers = 0;
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -24,54 +30,39 @@ namespace FTPCheck
 
         Program()
         {
+            try
+            {
+                okservers = File.ReadLines(outputFile).Count();
+            }
+            catch
+            {
+                okservers = 0;
+            }
             Random r = new Random();
             lines = File.ReadLines(inputFile);
             Console.Out.WriteLine(lines.Count() + " " + lines.ElementAt(lines.Count()-1));
-            //Thread myNewThread = new Thread(() => testServer("localhost"));
-            //myNewThread.Start();
-            for(int i = 0; i<lines.Count(); i=i+8)
+            for (int i = 1; i < lines.Count(); i++)
             {
                 Thread thread1 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread2 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread3 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread4 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread5 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread6 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread7 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread8 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread9 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread10 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread11 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread12 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread13 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread14 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread15 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-                Thread thread16 = new Thread(() => testServer(lines.ElementAt(r.Next(0, lines.Count()))));
-
                 thread1.Start();
-                thread2.Start();
-                thread3.Start();
-                thread4.Start();
-                thread5.Start();
-                thread6.Start();
-                thread7.Start();
-                thread8.Start();
-                thread9.Start();
-                thread10.Start();
-                thread11.Start();
-                thread12.Start();
-                thread13.Start();
-                thread14.Start();
-                thread15.Start();
-                thread16.Start();
-                Thread.Sleep(5000);
-                Console.Out.WriteLine(i);
+                Thread.Sleep(1);
+                if(i % THREAD_AMOUNT == 0)
+                {
+                    Properties.Settings.Default["ServersChecked"] = Convert.ToInt32(Properties.Settings.Default["ServersChecked"].ToString()) + THREAD_AMOUNT;
+                    Properties.Settings.Default.Save();
+                    Console.Write(Properties.Settings.Default["ServersChecked"].ToString() + " ", Color.Yellow);
+                    Console.WriteLine(okservers, Color.Lime);
+                    Thread.Sleep(5000);
+                }
             }
-            
-            
-            Thread.Sleep(10000);
-        }
 
+
+                Thread.Sleep(10000);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ip"></param>
         public void testServer(string ip)
         {
             FTPConnection ftp = new FTPConnection();
@@ -82,7 +73,7 @@ namespace FTPCheck
                 ftp.ServerAddress = ip;
                 ftp.UserName = "anonymous";
                 ftp.Password = "anonymous@";
-                ftp.Timeout = 10000;
+                ftp.Timeout = 5000;
                 current = "connecting";
                 ftp.Connect();
                 current = "uploading";
@@ -92,7 +83,8 @@ namespace FTPCheck
                 {
                     current = "deleting";
                     ftp.DeleteFile("test.txt");
-                    Console.WriteLine("OK: " + ip, Color.LightGreen);
+                    Console.WriteLine("OK: " + ip, Color.Lime);
+                    okservers++;
                     using (StreamWriter w = File.AppendText(outputFile))
                     {
                         w.WriteLine(ip);
@@ -107,7 +99,7 @@ namespace FTPCheck
             }
             catch (Exception ex)
             {
-                Console.Out.WriteLine("Error: " + current +" " + ex.Message);
+                Console.WriteLine("Error: " + current +" " + ex.Message);
             }
             finally
             {
